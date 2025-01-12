dfe2954a400f91dbb5c4181f43dc9b11d915a1fc
mazhars-git <mazharulislam611@gmail.com>
2025-01-12 21:18:28 +0600

    reduce use

---
+++ test.js
@@ -0,0 +1,33 @@
+const adminPaths2 = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    element: "ADMIN_DASHBOARD",
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "/admin/create-admin",
+        element: "CREATE_ADMIN",
+      },
+      {
+        name: "Create Faculty",
+        path: "/admin/create-faculty",
+        element: "CREATE_FACULTY",
+      },
+      {
+        name: "Create Student",
+        path: "/admin/create-student",
+        element: "CREATE_STUDENT",
+      },
+    ],
+  },
+];
+
+const newArray = adminPaths2.reduce((acc, item) => {
+  acc.push(item);
+
+  return acc;
+}, []);
