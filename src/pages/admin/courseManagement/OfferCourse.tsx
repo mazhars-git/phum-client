3cb0683b9a8a900f993247f2d3319d7b6cbc3780
mazhars-git <mazharulislam611@gmail.com>
2025-01-30 00:25:44 +0600

    disabled input

--- src/pages/admin/courseManagement/OfferCourse.tsx
+++ src/pages/admin/courseManagement/OfferCourse.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import PHSelectWithWatch from "../../../components/form/PHSelectWithWatch";
+import { useState } from "react";
 
 const OfferCourse = () => {
+  const [id, setId] = useState("");
+
+  const { data: academicFacultyData } = useGetAcademicFacultiesQuery(undefined);
+  const academicSemesterOptions = academicFacultyData?.data?.map((item) => ({
+    value: item._id,
+    label: item.name,
+  }));
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h1>This is OfferCourse component</h1>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelectWithWatch
+            onValueChange={setId}
+            options={academicSemesterOptions}
+            name="academicSemester"
+            label="Academic Semester"
+          />
+          <PHInput disabled={!id} type="text" name="test" label="Test" />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
